7e3547a443d6bc3595f9c2917b0060fe819df883
GA253NH <GA253NH@IN3268364W1.ey.net>
2024-05-28 18:29:22 +0530

    testrunnercollection

---
+++ Test Suites/loginTestRunCollection.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>loginTestRunCollection</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>596c3da3-750c-4521-8d15-a13ed152aa4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>3b93b627-82cf-45d4-a5cc-e36e65ff29da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/loginRunTime</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
